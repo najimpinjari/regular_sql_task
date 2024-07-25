b062fb4961ebead6c4a89ac74da2a394fbbbcb17
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-07-25 14:28:42 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,38 @@
+// //if 
+// let age : number = 24;
+// if(age >= 18){
+//     console.log("are you single ? ");
+// }
+// // if else 
+// let temprature : number = 10;
+// if (temprature = 20)
+// {
+//     console.log('its ten')
+// }else{
+//     console.log('its twenty ');
+// }
+// //if else if else statment 
+// let mark : number = 40;
+// if (mark >= 35){
+//     console.log('you are pass');
+// }else if(mark >= 50){
+//     console.log('very good ');
+// }else{
+//     console.log('you need to improve .');
+// }
+// let isstudent : boolean = true;
+// let message : string = isstudent ? "are you student" : "you are not student";
+// console.log(message);
+var day = 3;
+var dayName;
+switch (day) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    default:
+        dayName = "Unknown day";
+}
+console.log(dayName);
